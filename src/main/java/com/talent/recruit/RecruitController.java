5c661caad2f29beac861cc8f9182607c645b640e
AjaiGuvaliour <ajairama261095@gmail.com>
2019-06-07 01:12:58 +0530

    link has been given for recurit pages

--- src/main/java/com/talent/recruit/RecruitController.java
+++ src/main/java/com/talent/recruit/RecruitController.java
@@ -4,16 +4,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
+@RequestMapping("/talent")
 public class RecruitController {
 @Autowired
 MprFormRepository formrepo;
 
-@GetMapping(value="/talent")
+@GetMapping(value="/recuritment")
 public String Recurit(Model model) {
+	
+	
+	return "recuritmentHome";
+}
+
+@GetMapping(value="/mprForm")
+public String mprForm(Model model) {
 	
 	
 	return "CreateMpr";
 }
+
+
+@GetMapping(value="/addCandidate")
+public String addCandidate(Model model) {
+	
+	
+	return "registeration";
+}
+
+
+@GetMapping(value="/trfStatus")
+public String trfStatus(Model model) {
+	
+	return "MPRstatus";
+}
+
+
+
+
+
+
 
 }
